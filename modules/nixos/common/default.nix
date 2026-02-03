39f849688c455064d66aea88d4a26291a6ef3746
MD. Sarwar Uddin <99182873+SrwR16@users.noreply.github.com>
2026-02-03 09:05:52 +0100

    Change timezone to Asia/Dhaka and update locales

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -82,22 +82,23 @@
     plymouth-quit-wait.enable = false;
   };
 
-  # Timezone
-  time.timeZone = "Europe/Warsaw";
+# Timezone
+time.timeZone = "Asia/Dhaka";
 
-  # Internationalization
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
-  };
+# Internationalization
+i18n.defaultLocale = "en_US.UTF-8";
+i18n.extraLocaleSettings = {
+  LC_ADDRESS = "en_US.UTF-8";
+  LC_IDENTIFICATION = "en_US.UTF-8";
+  LC_MEASUREMENT = "en_US.UTF-8";
+  LC_MONETARY = "en_US.UTF-8";
+  LC_NAME = "en_US.UTF-8";
+  LC_NUMERIC = "en_US.UTF-8";
+  LC_PAPER = "en_US.UTF-8";
+  LC_TELEPHONE = "en_US.UTF-8";
+  LC_TIME = "en_US.UTF-8";
+};
+
 
   # Enables support for Bluetooth
   hardware.bluetooth = {
